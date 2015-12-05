f143f48cafd268d7521ea61935682c19a3ce5685
Christopher Chapman <christopher.chapman@disney.com>
2015-12-05 07:38:25 -0800

    starter code

---
+++ bookmarklet-external.js
@@ -0,0 +1,13 @@
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+	script = document.createElement( 'script' );
+	script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'; 
+	script.onload=doWork;
+	document.body.appendChild(script);
+} 
+else {
+    doWork();
+}
+ 
+function doWork() {
+    alert('works!');
+}
