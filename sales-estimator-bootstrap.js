0efbe2951a965cc311e2c3b6467128e2249cf765
Christopher Chapman <christopher.chapman@disney.com>
2016-03-30 07:29:34 -0700

    testing with headjs

---
+++ sales-estimator-bootstrap.js
@@ -0,0 +1,36 @@
+(function (window, document) {
+
+	if (typeof window.head=='undefined') {
+		// total overkill -- whatevs...  it's prolly cached.
+		script = document.createElement('script');
+		script.src = 'http://cdnjs.cloudflare.com/ajax/libs/headjs/1.0.3/head.min.js';
+		script.onload = preload;
+		document.body.appendChild(script);
+	} else {
+		preload();
+	}
+
+	var preload = function() {
+		//"Help us improve our Author Pages";
+		head.ready(function () {
+            validate();
+        });
+
+		head.load(
+	    	"https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"
+		);
+
+	}
+
+	var validate = function() {
+		// look for: "Help us improve our Author Pages";
+		// says it's an author page.
+		// else, look for sales rank.
+		// says it's a book page.
+		// if neither, pop up the amazon link for sherlock holmes.
+		alert('working! ' + $("#SalesRank").text());
+	}
+
+
+
+}(this, this.document));
